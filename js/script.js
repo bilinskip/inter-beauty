4e7b27db292b35deeab87b632121959668198f58
bilinskip <p.bilinski22@gmail.com>
2020-11-07 16:33:03 +0100

    07.11

--- js/script.js
+++ js/script.js
@@ -1,43 +1,36 @@
 var images;
-function loadImages(){
-  var list = document.getElementsByClassName("slideshow-container").item(0);
-  var dots = document.getElementsByClassName("dots").item(0);
-  console.log('list ', list);
-  images.forEach((item) => {
-    var li = document.createElement('div');
-    li.setAttribute('class', 'mySlides fade');
-    //li.innerHTML = '<img src = "./img/gallery/' + item +'.jpg" class="gallery-img"/>';
-    li.innerHTML = '<div class="numbertext">'+item +' / ' + images.length +'</div><img src="./img/gallery/' + item +'.jpg" style="width:100" class="gallery-img">';
-    list.appendChild(li);
-    var dot = document.createElement('span');
-    dot.setAttribute('class', 'dot');
-    dot.setAttribute('onclick', 'currentSlide(' + item +')');
-    dots.appendChild(dot);
-  }); 
-  
-}
+var slideIndex = 1;
 
 fetch('./img/gallery/files.txt')
   .then(response => response.text())
   .then((data) => {
-    console.log(data);
     images = data.split(';');
-    console.log('## images ', images);
     loadImages();
+    showDivs(slideIndex);
   });
 
-  var myIndex = 0;
-  carousel();
+function loadImages(){
+  var list = document.getElementsByClassName("carousel-container").item(0);
+  images.forEach((item) => {
+  var img = document.createElement('img');
+  img.setAttribute('src', './img/gallery/' + item +'.jpg');
+  img.setAttribute('class', 'gallery-img');
+  list.appendChild(img);
+  }); 
   
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-  }
+}
 
+function plusSlides(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("gallery-img");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
